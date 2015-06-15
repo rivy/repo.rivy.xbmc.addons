8c847a8a8a4f61d292308b9fb0e5c2e550a4cf12
Roy Ivy III <RIVY.dev@gmail.com>
2015-06-15 16:06:53 -0500

    CHANGE: update python script to be python2 or python3 compatible

--- addons_gen.py
+++ addons_gen.py
@@ -2,8 +2,13 @@
 
 """ addons.xml generator """
 
+# Python v2/3 compatiblity
+# URLref: [__future__ imports] http://python-future.org/imports.html @@ http://archive.is/yHfDA @@ http://webcitation.org/6NDj2zuHH
+# URLref: [Should I import unicode_literals?] http://python-future.org/imports.html#unicode-literals
+from __future__ import absolute_import, division, print_function #, unicode_literals
+
 import os
-import md5
+import hashlib
 
 
 class Generator:
@@ -17,7 +22,7 @@ class Generator:
         self._generate_addons_file()
         self._generate_md5_file()
         # notify user
-        print "Finished updating addons xml and md5 files"
+        print ( "Finished updating addons xml and md5 files" )
 
     def _generate_addons_file( self ):
         # addon list
@@ -27,12 +32,13 @@ class Generator:
         # loop thru and add each addons addon.xml file
         for addon in addons:
             try:
-                # skip any file or .svn folder
+                # skip VCS folders
                 if ( not os.path.isdir( addon ) or addon == ".svn" or addon == ".git" or addon == ".hg" ): continue
                 # create path
                 _path = os.path.join( addon, "addon.xml" )
-                # split lines for stripping
-                xml_lines = open( _path, "r" ).read().splitlines()
+                with open( _path, "r" ) as f:
+                    # split lines for stripping
+                    xml_lines = f.read().splitlines()
                 # new addon
                 addon_xml = ""
                 # loop thru cleaning each line
@@ -40,34 +46,37 @@ class Generator:
                     # skip encoding format line
                     if ( line.find( "<?xml" ) >= 0 ): continue
                     # add line
-                    addon_xml += unicode( line.rstrip() + "\n", "UTF-8" )
+                    addon_xml += line.rstrip() + "\n"
                 # we succeeded so add to our final addons.xml text
                 addons_xml += addon_xml.rstrip() + "\n\n"
-            except Exception, e:
+            except Exception as e:
                 # missing or poorly formatted addon.xml
-                print "Excluding %s for %s" % ( _path, e, )
+                print ( "Excluding %s for %s" % ( _path, e, ) )
+
         # clean and add closing tag
         addons_xml = addons_xml.strip() + u"\n</addons>\n"
         # save file
-        self._save_file( addons_xml.encode( "UTF-8" ), file="addons.xml" )
+        self._save_file( addons_xml, file="addons.xml" )
 
     def _generate_md5_file( self ):
-        try:
-            # create a new md5 hash
-            m = md5.new( open( "addons.xml" ).read() ).hexdigest()
-            # save file
-            self._save_file( m, file="addons.xml.md5" )
-        except Exception, e:
-            # oops
-            print "An error occurred creating addons.xml.md5 file!\n%s" % ( e, )
+        with open ( "addons.xml", "rb" ) as f:
+            try:
+                # create a new md5 hash
+                m = hashlib.md5()
+                m.update( f.read() )
+                # save file
+                self._save_file( m.hexdigest(), file="addons.xml.md5" )
+            except Exception as e:
+                # oops
+                print ( "An error occurred creating addons.xml.md5 file!\n%s" % ( e, ) )
 
-    def _save_file( self, data, file ):
+    def _save_file( self, data, file, mode="w" ):
         try:
             # write data to the file
-            open( file, "w" ).write( data )
-        except Exception, e:
+            open( file, mode ).write( data )
+        except Exception as e:
             # oops
-            print "An error occurred saving %s file!\n%s" % ( file, e, )
+            print ("An error occurred saving %s file!\n%s" % ( file, e, ))
 
 
 if ( __name__ == "__main__" ):
